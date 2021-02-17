ebb34b1a1fa67756be5688f43d580e9ca9182006
PC-coding <partapchauhan8@gmail.com>
2021-02-16 19:06:32 -0500

    shows image in app and closes

--- face_detection.py
+++ face_detection.py
@@ -6,4 +6,10 @@ trained_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # choosing image to detect faces in
 img = cv2.imread('Cillian.png')
 
+# displaying images + pauses the python program with the image for viewing
+# clicking any key will close the app
+# message will appear
+cv2.imshow('Face Detector', img)
+cv2.waitKey()
+
 print('success')
