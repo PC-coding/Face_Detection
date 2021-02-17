b18da3d02781697dc61193e366e224a210fd8c80
PC-coding <partapchauhan8@gmail.com>
2021-02-17 14:28:12 -0500

    new pic + added loop for multiple faces

--- face_detection.py
+++ face_detection.py
@@ -14,12 +14,13 @@ face_detect = trained_data.detectMultiScale(grayscale_img)
 print(face_detect)
 
 # setting coordinates to detect the first face from top left to bottom right
-(x, y, w, h) = face_detect[0]
+ # (x, y, w, h) = face_detect[0]
 
 # rectangular boxes identifying faces 
 # coordinates set to x,y,w,h and color set to red with a border thickness of 7 
-# cv2.rectangle(img, (150, 212), (920, 920+212), (0, 255, 0), 2)
-cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 225), 7)
+ # cv2.rectangle(img, (150, 212), (920, 920+212), (0, 255, 0), 2)
+for (x, y, w, h) in face_detect:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 225), 7)
 
 # displaying images + pauses the python program with the image for viewing
 # clicking any key will close the app
