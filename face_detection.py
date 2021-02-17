49638d21ca05351e23810d39333e66289f70e578
PC-coding <partapchauhan8@gmail.com>
2021-02-16 19:15:21 -0500

    conversion to grayscale

--- face_detection.py
+++ face_detection.py
@@ -6,6 +6,9 @@ trained_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # choosing image to detect faces in
 img = cv2.imread('Cillian.png')
 
+# conversion to grayscale
+grayscale_img = cv2.cvtColor(img, cv2.COLOR_BG2GRAY)
+
 # displaying images + pauses the python program with the image for viewing
 # clicking any key will close the app
 # message will appear
