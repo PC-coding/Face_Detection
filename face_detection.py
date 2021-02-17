a0890e2121b7e48d5fe8a860c41f780aa4c8b412
PC-coding <partapchauhan8@gmail.com>
2021-02-16 19:06:15 -0500

    adding image for detecting faces

--- face_detection.py
+++ face_detection.py
@@ -1,5 +1,9 @@
 import cv2
 
-
 # load pre-trained data on face frontals from opencv
 trained_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# choosing image to detect faces in
+img = cv2.imread('Cillian.png')
+
+print('success')
