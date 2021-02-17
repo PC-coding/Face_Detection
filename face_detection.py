7172afd8695596579ba7bfa38e5df0ff64b33039
PC-coding <partapchauhan8@gmail.com>
2021-02-16 19:16:36 -0500

    detection algorithm

--- face_detection.py
+++ face_detection.py
@@ -9,6 +9,10 @@ img = cv2.imread('Cillian.png')
 # conversion to grayscale
 grayscale_img = cv2.cvtColor(img, cv2.COLOR_BG2GRAY)
 
+# detection algorithm
+face_detect = trained_data.detectMultiScale(grayscale_img)
+
+
 # displaying images + pauses the python program with the image for viewing
 # clicking any key will close the app
 # message will appear
