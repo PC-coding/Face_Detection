a861558d7d8244ace40ca9027598efaae0c67941
PC-coding <partapchauhan8@gmail.com>
2021-02-16 19:41:00 -0500

    adding rectangular boxes to identify faces

--- face_detection.py
+++ face_detection.py
@@ -7,11 +7,15 @@ trained_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 img = cv2.imread('Cillian.png')
 
 # conversion to grayscale
-grayscale_img = cv2.cvtColor(img, cv2.COLOR_BG2GRAY)
+grayscale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # detection algorithm
 face_detect = trained_data.detectMultiScale(grayscale_img)
+print(face_detect)
 
+# rectangular boxes identifying faces 
+cv2.rectangle(img, (150, 212), (920, 920+212), (0, 255, 0), 2)
+#cv2.rectangle(img, (x+y), (x+w, y+h), (0, 255, 0), 2)
 
 # displaying images + pauses the python program with the image for viewing
 # clicking any key will close the app
